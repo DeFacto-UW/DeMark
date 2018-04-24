cfc232e0773efb62914ed5953ff7de104b3f2dd5
Tuan Ma <tuanma123@gmail.com>
2018-04-24 10:10:07 -0700

    Added test to see if travis is running.

---
+++ DeMark/src/tests/TravisTest.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TravisTest {
+
+    @Test
+    public void travisIsWorking() {
+        assertTrue(true);
+    }
+}
