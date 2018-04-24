37c10ed0393d6af3741f17832760fa63f7bd6125
Tony Vo <tonyvo@cs.washington.edu>
2018-04-24 11:04:33 -0700

    Need to implement batch selection bookmarking

--- DeMark/src/main/java/ClearAction.java
+++ DeMark/src/main/java/ClearAction.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import com.intellij.codeInsight.highlighting.HighlightManager;
 import com.intellij.ide.bookmarks.Bookmark;
 import com.intellij.ide.bookmarks.BookmarkManager;
 import com.intellij.openapi.actionSystem.AnAction;
@@ -8,7 +9,10 @@ import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.highlighter.EditorHighlighter;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
 
 import java.util.List;
 
@@ -20,17 +24,23 @@ public class ClearAction extends AnAction {
             return;
         }
 
+        // Bookmark Manager deals with bookmarks for this file
+        // Document is this current file
+        // Editor is the IntelliJ editor
         BookmarkManager bookmarkManager = BookmarkManager.getInstance(project);
         Document document = anActionEvent.getData(LangDataKeys.EDITOR).getDocument();
         Editor editor = anActionEvent.getData(LangDataKeys.EDITOR);
-
         List<Bookmark> bookmarkList = bookmarkManager.getValidBookmarks();
 
-        for (Bookmark bookmark : bookmarkList) {
+        // Get the corresponding file to this document
+        VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(document);
+        String thisFileName = virtualFile.getName();
 
-            // TODO: Check file name
+        for (Bookmark bookmark : bookmarkList) {
+            String bookmarkFileName = bookmark.getFile().getName();
+
             // Remove all bookmarks with Demark in this file
-            if (bookmark.getDescription().equals("Demark")) {
+            if (bookmark.getDescription().equals("Demark") && thisFileName.equals(bookmarkFileName)) {
                 int lineNum = bookmark.getLine();
                 bookmarkManager.removeBookmark(bookmark);
 
@@ -38,8 +48,8 @@ public class ClearAction extends AnAction {
                 int startPos = document.getLineStartOffset(lineNum);
                 int endPos = document.getLineEndOffset(lineNum);
 
-                // Remove the line
-                Runnable removeText = () -> document.deleteString(startPos, endPos);
+                // Remove the line content
+                Runnable removeText = () -> document.deleteString(startPos, endPos + 1);
                 WriteCommandAction.runWriteCommandAction(project, removeText);
             }
         }
--- DeMark/src/main/java/MarkAction.java
+++ DeMark/src/main/java/MarkAction.java
@@ -1,5 +1,6 @@
 package main.java;
 
+import com.intellij.codeInsight.highlighting.HighlightManager;
 import com.intellij.ide.bookmarks.Bookmark;
 import com.intellij.ide.bookmarks.BookmarkManager;
 import com.intellij.openapi.actionSystem.AnAction;
@@ -7,15 +8,16 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 
-import java.awt.print.Book;
+import java.util.List;
+
 
 public class MarkAction extends AnAction {
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
-
         Project project = anActionEvent.getProject();
         if (project == null) {
             return;
@@ -25,11 +27,38 @@ public class MarkAction extends AnAction {
         Document document = anActionEvent.getData(LangDataKeys.EDITOR).getDocument();
         Editor editor = anActionEvent.getData(LangDataKeys.EDITOR);
 
-        int currLineNum = document.getLineNumber(editor.getSelectionModel().getSelectionStart());
+        // Get the current open file
+        VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(document);
+        String thisFileName = virtualFile.getName();
 
-        // TODO: Add bookmark description DeMark
-        bookmarkManager.addEditorBookmark(editor, currLineNum);
-        Bookmark added = bookmarkManager.findEditorBookmark(document, currLineNum);
-        bookmarkManager.setDescription(added, "Demark");
+
+        // TODO: Try and highlight a line??
+        // TODO: Handle batch selection bookmarking
+//        int startPos = document.getLineStartOffset(currLineNum);
+//        int endPos = document.getLineEndOffset(currLineNum);
+//        HighlightManager.getInstance(project).addOccurrenceHighlight(editor, startPos, endPos,);
+
+        boolean removed = false;
+
+        int currLineNum = document.getLineNumber(editor.getSelectionModel().getSelectionStart());
+        // Remove bookmark if already removed
+        List<Bookmark> bookmarkList = bookmarkManager.getValidBookmarks();
+        for (Bookmark bookmark : bookmarkList) {
+            int lineNum = bookmark.getLine();
+            String bookmarkFileName = bookmark.getFile().getName();
+
+            // Line is already contained
+            if (lineNum == currLineNum && bookmark.getDescription().equals("Demark") && thisFileName.equals(bookmarkFileName)) {
+                bookmarkManager.removeBookmark(bookmark);
+                removed = true;
+            }
+        }
+
+        // Add bookmark with description Demark
+        if (!removed) {
+            bookmarkManager.addEditorBookmark(editor, currLineNum);
+            Bookmark added = bookmarkManager.findEditorBookmark(document, currLineNum);
+            bookmarkManager.setDescription(added, "Demark");
+        }
     }
 }
