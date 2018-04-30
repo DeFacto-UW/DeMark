c6d1ee11019295d590d37dfce79f107612f387b5
Tony Vo <tonyvo@uw.edu>
2018-04-30 16:26:56 -0700

    Reimplementing mark action to mark multiple lines

--- DeMark/src/main/java/MarkAction.java
+++ DeMark/src/main/java/MarkAction.java
@@ -25,7 +25,6 @@ import java.util.List;
 public class MarkAction extends AnAction {
 
     private BookmarkManager bookmarkManager;
-
     private Document document;
     private Editor editor;
 
@@ -50,39 +49,81 @@ public class MarkAction extends AnAction {
         VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(document);
         String thisFileName = virtualFile.getName();
 
-        int currentLine = document.getLineNumber(editor.getSelectionModel().getSelectionStart());
+        // Find the line start positions of selected text
+        ArrayList<Integer> lineStarts = getSelectionStarts();
+        int countMarked = 0;
 
-        // TODO: Handle batch selection bookmarking
+        // Count the number of lines marked
+        for (int i = 0; i < lineStarts.size(); i++) {
+            int lineNum = document.getLineNumber(lineStarts.get(i));
+           if (lineAlreadyMarked(lineNum, thisFileName)) {
+              countMarked++;
+           }
+        }
 
-        // ISSUE: Bookmark persists through sessions but highlight does not.
+        // Toggle on and off bookmarks
+        for (int i = 0; i < lineStarts.size(); i++) {
+            int lineNum = document.getLineNumber(lineStarts.get(i));
 
-        boolean removed = false;
+            if (countMarked != lineStarts.size()) {
+                if (!lineAlreadyMarked(lineNum, thisFileName)) {
+                    addDemarkBookmark(lineNum);
+                }
+            } else {
+                removeDemarkBook(lineNum, thisFileName);
+            }
+        }
+    }
 
-        // Remove bookmark if already exists
+    /**
+     *
+     * @param lineNum, the line to remove the Demark bookmark from
+     * @param fileName, the name of the file that contains the Demark bookmark (ie. currently shown)
+     */
+    private void removeDemarkBook(int lineNum, String fileName) {
+        List<Bookmark> bookmarkList = bookmarkManager.getValidBookmarks();
+        for (Bookmark bookmark : bookmarkList) {
+            int currLine = bookmark.getLine();
+            String bookmarkFileName = bookmark.getFile().getName();
+
+            // Line is already contained
+            if (currLine == lineNum && bookmark.getDescription().equals("DeMark") && fileName.equals(bookmarkFileName)) {
+                removeHighlight(lineNum);
+                bookmarkManager.removeBookmark(bookmark);
+            }
+        }
+    }
+
+    // Add a DeMark bookmark and add highlighting
+    private void addDemarkBookmark(int lineNum) {
+        bookmarkManager.addEditorBookmark(editor, lineNum);
+        Bookmark added = bookmarkManager.findEditorBookmark(document, lineNum);
+        if (added != null) {
+            bookmarkManager.setDescription(added, "DeMark");
+
+        }
+        addHighlight(lineNum);
+    }
+
+    /**
+     * Determine if the a line is already marked by Demark plugin
+     * @param lineChecked, the line to cross check
+     * @param filename, the file name that the line is in
+     * @return true if line contained, false otherwise
+     */
+    private boolean lineAlreadyMarked(int lineChecked, String filename) {
         List<Bookmark> bookmarkList = bookmarkManager.getValidBookmarks();
         for (Bookmark bookmark : bookmarkList) {
             int lineNum = bookmark.getLine();
             String bookmarkFileName = bookmark.getFile().getName();
 
             //TODO: Optimize this so we don't have nested loops
-           
             // Line is already contained
-            if (lineNum == currentLine && bookmark.getDescription().equals("DeMark") && thisFileName.equals(bookmarkFileName)) {
-                removeHighlight(currentLine);
-                bookmarkManager.removeBookmark(bookmark);
-                removed = true;
+            if (lineChecked == lineNum && bookmark.getDescription().equals("DeMark") && filename.equals(bookmarkFileName)) {
+                return true;
             }
         }
-
-        // Add bookmark with description "DeMark"
-        if (!removed) {
-            bookmarkManager.addEditorBookmark(editor, currentLine);
-            Bookmark added = bookmarkManager.findEditorBookmark(document, currentLine);
-            if (added != null) {
-                bookmarkManager.setDescription(added, "DeMark");
-            }
-            addHighlight(currentLine);
-        }
+        return false;
     }
 
     // initializes all fields
@@ -102,6 +143,7 @@ public class MarkAction extends AnAction {
 
     // removes a highlight on the current line
     private void removeHighlight(int currentLine) {
+        // Offset of a line
         int offset = DocumentUtil.getFirstNonSpaceCharOffset(editor.getMarkupModel().getDocument(), currentLine);
         for (RangeHighlighter highlighter : highlighters) {
             if (highlighter.getStartOffset() == offset &&
@@ -119,5 +161,33 @@ public class MarkAction extends AnAction {
 
         RangeHighlighter highlighter = editor.getMarkupModel().addLineHighlighter(currentLine, HighlighterLayer.LAST - 1, ta);
         highlighters.add(highlighter);
+    }
+
+    /**
+     * Find the start positions of each line of the editors currently selected text
+     * @return ArrayList of line start positions
+     */
+    private ArrayList<Integer> getSelectionStarts() {
+        // Character position of selected model
+        int selectPosStart = editor.getSelectionModel().getSelectionStart();
+        int selectPosEnd = editor.getSelectionModel().getSelectionEnd();
+
+        int currPos = selectPosStart;
+        ArrayList<Integer> lineStarts = new ArrayList<>();
+
+        // Add all line starts positions to the array
+        while (currPos <= selectPosEnd) {
+            lineStarts.add(currPos);
+            int lineNum = document.getLineNumber(currPos);
+
+            // Calculate the offset from start of line to end
+            int startOffset = document.getLineStartOffset(lineNum);
+            int endOffset = document.getLineEndOffset(lineNum);
+
+            // Position of next line is 1 over currPos + offSet
+            int offSet = endOffset - startOffset;
+            currPos += offSet + 1;
+        }
+        return lineStarts;
     }
 }
