6f9b9e3eeadff58cef14075edde6fa6cdcf4806d
Tuan Ma <tuanma123@gmail.com>
2018-04-30 15:52:03 -0700

    Added an example complex test data code file.

---
+++ DeMark/src/tests/java/testData/MultiPrintln.java
@@ -0,0 +1,22 @@
+package testData;
+
+public class MultiPrintln {
+    public static int foo() {
+        int x = 5;
+        int y = 5;
+        int z = x + y;
+        System.out.println(z);
+        return z;
+    }
+
+    public static String bar(int y) {
+        System.out.println(22);
+        boolean isEven = y % 2 == 0;
+        return "Return " + y;
+    }
+
+    public static void baz() {
+        System.out.print("Hello ");
+        System.out.println("world");
+    }
+}
