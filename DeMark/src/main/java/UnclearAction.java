28b4636f765c4e4891632bf0d0b8166062ed6ad8
Tuan Ma <tuanma123@gmail.com>
2018-04-30 14:49:04 -0700

    Added documentation that states display and unclear are not yet implemented.

--- DeMark/src/main/java/UnclearAction.java
+++ DeMark/src/main/java/UnclearAction.java
@@ -2,10 +2,19 @@ package main.java;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import exceptions.NotYetImplementedException;
 
 public class UnclearAction extends AnAction {
+    /**
+     * Not yet implemented.
+     * @param anActionEvent
+     */
     @Override
     public void actionPerformed(AnActionEvent anActionEvent) {
-
+        try {
+            throw new NotYetImplementedException();
+        } catch (NotYetImplementedException e) {
+            e.printStackTrace();
+        }
     }
 }
