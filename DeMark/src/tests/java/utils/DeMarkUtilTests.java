ee4d13e165ab3bcacdbde6a6039de061fe949aa0
Andrew <andremail03@gmail.com>
2018-05-13 16:53:09 -0700

    Added two more tests for DemarkUtil

--- DeMark/src/tests/java/utils/DeMarkUtilTests.java
+++ DeMark/src/tests/java/utils/DeMarkUtilTests.java
@@ -10,11 +10,15 @@ import org.junit.Test;
 import tests.java.TestingUtility;
 import main.java.utils.DemarkUtil;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class DeMarkUtilTests extends LightCodeInsightFixtureTestCase {
     Editor editor;
     Document document;
+
+    int lineNum;
 
     @Before
     public void setUp() throws Exception {
@@ -23,6 +27,8 @@ public class DeMarkUtilTests extends LightCodeInsightFixtureTestCase {
 
         editor = myFixture.getEditor();
         document = myFixture.getEditor().getDocument();
+
+        lineNum = TestingUtility.getCurrentCaretLine(myFixture);
     }
 
     @Override
@@ -31,8 +37,6 @@ public class DeMarkUtilTests extends LightCodeInsightFixtureTestCase {
     }
 
     public void testAddDemarkBookmarkCorrectLine() {
-        int lineNum = TestingUtility.getCurrentCaretLine(myFixture);
-
         DemarkUtil.addDemarkBookmark(editor, lineNum);
 
         List<Bookmark> demarkBookmarks = TestingUtility.getDeMarkBookmarks(myFixture);
@@ -40,11 +44,40 @@ public class DeMarkUtilTests extends LightCodeInsightFixtureTestCase {
     }
 
     public void testAddDemarkBookmarkSuccess() {
-        int lineNum = TestingUtility.getCurrentCaretLine(myFixture);
-
         DemarkUtil.addDemarkBookmark(editor, lineNum);
 
         List<Bookmark> demarkBookmarks = TestingUtility.getDeMarkBookmarks(myFixture);
         assertEquals("Number of bookmarks incorrect", 1, demarkBookmarks.size());
+    }
+
+    public void testAddDemarkBookmarkCorrectDescription() {
+        DemarkUtil.addDemarkBookmark(editor, lineNum);
+
+        List<Bookmark> demarkBookmarks = TestingUtility.getDeMarkBookmarks(myFixture);
+
+        String description = demarkBookmarks.get(0).getDescription();
+        assertEquals("Wrong description set for bookmark", DemarkUtil.DEMARK_INDICATOR, description);
+    }
+
+    public void testAddDemarkBookmarkTwoLinesCorrect() {
+        DemarkUtil.addDemarkBookmark(editor, lineNum);
+        TestingUtility.shiftCaretLine(myFixture, 3, false, true);
+
+        int newLine = TestingUtility.getCurrentCaretLine(myFixture);
+        DemarkUtil.addDemarkBookmark(editor, newLine);
+
+        List<Bookmark> demarkBookmarks = TestingUtility.getDeMarkBookmarks(myFixture);
+        assertEquals("More than 2 bookmarks were set", 2, demarkBookmarks.size());
+
+        List<Integer> lines = new ArrayList<>();     // assuming only two bookmarks are present
+        lines.add(lineNum);
+        lines.add(newLine);
+
+        for (Bookmark bookmark : demarkBookmarks) {
+            assertTrue("One of the line numbers are wrong", lines.contains(bookmark.getLine()));
+
+            String description = demarkBookmarks.get(0).getDescription();
+            assertEquals("One of the description is wrong", DemarkUtil.DEMARK_INDICATOR, description);
+        }
     }
 }
