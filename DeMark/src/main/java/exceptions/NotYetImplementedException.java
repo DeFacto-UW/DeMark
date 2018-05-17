311d439c7f2dbe0e8d1b085426bc0d1b9f116f0f
Anh Nam Tran <atran35@cs.washington.edu>
2018-05-17 15:21:01 -0700

    Added documentation to all classes in main.java.exceptions

--- DeMark/src/main/java/exceptions/NotYetImplementedException.java
+++ DeMark/src/main/java/exceptions/NotYetImplementedException.java
@@ -1,5 +1,8 @@
 package exceptions;
 
+/**
+ * Exception class to help with writing tests before the tested function is implemented
+ */
 public class NotYetImplementedException extends Exception {
     /**
      * Constructor to implement a custom Java exception that represents a functionality/method that is not yet
