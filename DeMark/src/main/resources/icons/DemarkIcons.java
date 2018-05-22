a9eb885d440dec589179acf7e672c9d8c2705a87
Andrew Tran <andremail03@gmail.com>
2018-05-21 23:49:28 -0700

    Changed what the plugin uses for Toggle icon.

---
+++ DeMark/src/main/resources/icons/DemarkIcons.java
@@ -0,0 +1,11 @@
+package icons;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface DemarkIcons {
+    // icon by <div>Icons made by Gregor Cresnar from Flaticon
+    // licensed by Creative Commons BY 3.0
+    Icon TOGGLE = IconLoader.getIcon("/icons/eye.png");
+}
