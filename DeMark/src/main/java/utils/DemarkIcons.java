d6729221ae6d241805903741c2a0605c3026db14
Andrew Tran <andremail03@gmail.com>
2018-05-21 22:27:06 -0700

    Added interface for working with icons better.

---
+++ DeMark/src/main/java/utils/DemarkIcons.java
@@ -0,0 +1,12 @@
+package utils;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+/**
+ * Interface used primarily to get and store icons for DeMark
+ */
+public interface DemarkIcons {
+    final Icon TOGGLE = IconLoader.getIcon("main/resources/icons/toggle.png");
+}
