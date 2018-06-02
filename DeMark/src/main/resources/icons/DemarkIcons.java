3b3d4137c1e986b248a36a72e45fbdea4a93fb43
Andrew Tran <andremail03@gmail.com>
2018-06-01 17:40:26 -0700

    Added docs to DemarkIcons.java.

--- DeMark/src/main/resources/icons/DemarkIcons.java
+++ DeMark/src/main/resources/icons/DemarkIcons.java
@@ -4,6 +4,9 @@ import com.intellij.openapi.util.IconLoader;
 
 import javax.swing.*;
 
+/**
+ * Icon interface used to get the correct icons for the plugin.
+ */
 public interface DemarkIcons {
     // icon by <div>Icons made by Gregor Cresnar from Flaticon
     // licensed by Creative Commons BY 3.0
